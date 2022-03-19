921e3970b8f5af3006ce25e73901cea13644ca95
Rohit kumar <rohitjadav1729@gmail.com>
2022-03-19 21:44:08 +0530

    add og: meta tags and preview image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid"
 import { useEffect } from "react"
+import { Head } from "next/head"
 
 const uniqueIds = []
 export default function Home() {
@@ -24,6 +25,31 @@ export default function Home() {
 
   return (
     <div className="tw-min-h-screen tw-m-0">
+      <Head>
+        <title>
+          High End Modern UI components made with Nextjs & tailwindCSS
+        </title>
+        <meta
+          name="description"
+          content="These are some of the examples of Modern UI components made with tailwindcss. all the designs are responsive."
+        />
+        <meta
+          property="og:title"
+          content="High End Modern UI components made with Nextjs & tailwindCSS"
+        />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/react-burger1-7e687.appspot.com/o/components-cover.png?alt=media&token=1443e3b4-f882-4182-86df-802c1b7e743f"
+        />
+        <meta
+          property="og:description"
+          content="These are some of the examples of Modern UI components made with tailwindcss. all the designs are responsive."
+        />
+        <meta
+          property="og:url"
+          content="https://v1-moderui-showcase.vercel.app"
+        />
+      </Head>
       <h1 className="tw-max-w-lg tw-my-4 tw-capitalize tw-text-purple-600 tw-font-semibold tw-mx-auto tw-p-2 tw-rounded tw-shadow-purple-100 tw-shadow-sm">
         I have created created replica of designs which I liked on internet and
         coded them. This project is made with Nextjs and tailwind CSS.
